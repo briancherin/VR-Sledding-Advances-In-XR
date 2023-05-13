6e050add8daea27a4c7a6e12b8fe64dfb7a20656
Aayushi Roy <aroy2530@umd.edu>
2023-05-12 23:30:54 -0400

    turning too

--- Assets/Scripts/Turning.cs
+++ Assets/Scripts/Turning.cs
@@ -6,14 +6,9 @@ public class Turning : MonoBehaviour
 {
     [SerializeField] Transform leftHand;    // attach LeftHandControllerAnchor
     [SerializeField] Transform rightHand;   // attach RightHandControllerAnchor
-<<<<<<< HEAD
 
     Rigidbody body;
     float angular_velocity;
-=======
-    bool turnLeft;
-    bool turnRight;
->>>>>>> ed99212cb8b3874babab9657a3d70e39c5b4e595
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +22,6 @@ public class Turning : MonoBehaviour
     {
         Vector3 leftPosition = leftHand.position;
         Vector3 rightPosition = rightHand.position;
-<<<<<<< HEAD
 
         bool turnLeft = rightPosition.y - leftPosition.y > 0.09;
         bool turnRight = leftPosition.y - rightPosition.y > 0.09;
@@ -36,36 +30,20 @@ public class Turning : MonoBehaviour
 
         angular_velocity = 0.0f;
         if (turnLeft && transform.rotation.eulerAngles.y > -45.0)
-=======
-        
-        // note: if sticking with this movement plan, change to ~0.06 units
-        // if the left hand is higher by 0.09 units, report wanting to turn right
-        if (leftPosition.y - rightPosition.y > 0.09)
->>>>>>> ed99212cb8b3874babab9657a3d70e39c5b4e595
         {
             //angular_velocity -= 5.0f;
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left*4);
             Debug.Log("Turning left");
         }
-<<<<<<< HEAD
         if (turnRight/* && transform.rotation.eulerAngles.y < 45.0*/)
-=======
-        // if the right hand is higher by 0.09 units, report wanting to turn left
-        else if (rightPosition.y - leftPosition.y > 0.09)
->>>>>>> ed99212cb8b3874babab9657a3d70e39c5b4e595
         {
             //angular_velocity += 5.0f;
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right*4);
             Debug.Log("Turning right");
 
         }
-<<<<<<< HEAD
 
         if (transform.position.z > 200)
-=======
-        // don't report turning
-        else 
->>>>>>> ed99212cb8b3874babab9657a3d70e39c5b4e595
         {
             transform.position = new Vector3(0, 27.473f, -88.491f);
         }
